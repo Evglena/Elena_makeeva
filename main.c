cce551429251235cbd6b582ed5b8a8f5e69b9b01
Evglena <elena_makeeva98@mail.ru>
2016-12-06 00:33:30 +0400

    lab3

--- main.c
+++ main.c
@@ -3,23 +3,36 @@
 
 int main()
 {
-    double G,F,Y,a,x;
-    const double PI=3.14;
-    printf("Введите x \n");
-    scanf("%lf",&x);
+    char r;
+    double G,F,Y,a,x1,x2;
+    const double pi=3.14;
+    printf("Введите x: \n");
+    scanf ("%lf", &x);
 
-    printf("Введите a \n");
-    scanf("%lf",&a);
+    printf("Введите a: \n");
+    scanf ("%lf", &a);
 
+    printf("Введите букву выражения, которое xотите вычислить - G,F,Y) \n");
+    scanf ("%s", &r);
 
-    G = - (16 * (( pow(a,2) + 24 * a * x - 27 * (pow (x, 2))))) / (45 * (pow(a, 2)) - 29 * a * x + 4 * (pow(x, 2)));
-    F = - (atan(PI * (10 * (a * a) + 3 * a * x -30 * (x * x))));
-    Y = - (log(2 * pow(a, 2) + 19 * a * x + 9 * pow(x, 2) + 1))/log(10);
+    switch (r){
+        case 'G':
+            if ((45 * (pow(a, 2)) - 29 * a * x + 4 * (pow(x, 2))) !=0)
+                G= -(16 * (( pow(a,2) + 24 * a * x - 27 * (pow (x, 2))))) / (45 * (pow(a, 2)) - 29 * a * x + 4 * (pow(x, 2)));
+            printf("G = %lf \n", G);
+            break;
 
-    printf("G = %lf \n", G);
-    printf("F = %lf \n", F);
-    printf("Y = %lf \n", Y);
+        case 'F':
+            F=-(atan(pi * (10 * (a * a) + 13 * a * x -30 * (x * x))));
+            printf("F = %lf \n", F);
+            break;
+
+        case 'Y':
+            Y=(log(2 * pow(a, 2) + 19 * a * x + 9 * pow(x, 2) + 1))/log(10);
+            printf("Y = %lf \n", Y);
+            break;
+        default: printf("ne veren vvod function \n");
+    }
 
     return 0;
-
 }
