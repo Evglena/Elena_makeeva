9c9f87b502d5a37ce770ccf3d813f6a07d89464b
Evglena <elena_makeeva98@mail.ru>
2017-01-09 13:58:26 +0400

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> /*позволяет выводить текст на экран и читать его с клавиатуры*/
 #include <math.h>
-
 int main() {
-    double G, F, Y, a, x1, x2, b, min_value, max_value;
-    int i, j;
+    double G, F, Y, a, x1, x2, b, min_value, max_value ;
+    int  j;
     char r;
     const double pi = 3.14;
-    double pr = 1;
+    int pr = 1;
 
     nachalo:
 
-    printf("%s", "Введите  x1: ");
-    scanf("%lf", &x1);
+    printf("%s", "Введите  x1: "); /* s- символ */
+    scanf("%lf", &x1); /*сканирует значение с клавиатуры */
 
     printf("%s", "Введите x2 ");
     scanf("%lf", &x2);
@@ -23,17 +22,19 @@ int main() {
     scanf("%lf", &b);
 
 
-    double mas[i];
+    double mas[100];
     j = 0;
     max_value = 0;
     min_value = 0;
-    printf("Введите букву выражения, которое xотите вычислить - G,F,Y) \n");
+
+    printf("Введите букву выражения, которое хотите вычислить - G,F,Y) \n");
     scanf ("%s", &r);
 
     switch (r) {
         case 'G':
             while (x1 <= x2) {
-                if ((45 * (pow(a, 2)) - 29 * a * x1 + 4 * (pow(x1, 2))) >=0.000001 && (45 * (pow(a, 2)) - 29 * a * x1 + 4 * (pow(x1, 2)) <= -0.000001){
+                if ((45 * (pow(a, 2)) - 29 * a * x1 + 4 * (pow(x1, 2))) >=0.000001 && (45 * (pow(a, 2)) - 29 * a * x1 + 4 * (pow(x1, 2)) <= -0.000001))
+                {
                     G = -(16 * ((pow(a, 2) + 24 * a * x1 - 27 * (pow(x1, 2))))) /
                         (45 * (pow(a, 2)) - 29 * a * x1 + 4 * (pow(x1, 2)));
                     printf("X = %.3lf \t| Y = %.3lf\n", x1, G);
@@ -84,7 +85,7 @@ int main() {
                 if (Y > max_value) max_value = mas[j];
                 if (Y < min_value) min_value = mas[j];
                 j += 1;
-            } 
+            }
     printf(" Максимальное значение  = %9.3lf\n Минимальное значение = %9.3lf\n", max_value, min_value);
     break;
 
@@ -92,8 +93,8 @@ int main() {
              printf("ne verno vvedena function \n");
 
     }
-    printf("Вычислим еще раз? (1-да,2-нет) \n");
-    scanf("%lf", &pr);
+    printf("Вычислим еще раз? (1-да) \n");
+    scanf("%i", &pr);
     if (pr == 1)
         goto nachalo;
     return 0;
